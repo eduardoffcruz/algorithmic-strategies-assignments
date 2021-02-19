7e8a3b1bea720f1cec834240ac2f61bff046abfc
Eduardo Cruz <eduardoffcruz@gmail.com>
2021-02-19 10:48:25 +0000

    reading stdin input

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ def outln(n): #output line to stdout
 def get_zeros_matrix(size):
     return [([0]*size) for i in range(size)]
 
+def read_board_params():
+    line=(readln()).split(' ')
+    board_size=int(line[0]) #N
+    max_slide=int(line[1]) #M , represents the maximum number of slides allowed
+    return board_size,max_slide
+
+def read_board(size):
+    board=[]
+    for k in range(size):
+        board_line=[int(n) for n in (readln()).split(' ')]
+        board.append(board_line)
+    return board
+
 def print_board(board,size):
     for i in range(size):
             for j in range(size):
@@ -17,13 +30,20 @@ def print_board(board,size):
             stdout.write("\n")
 
 def main():
-    """
-    #para ler até o EOF
-    for line in stdin:
-        line=line.rstrip().split(' ')
-    """
+    num_testcases=int(readln())
+    for k in range(num_testcases):
+        #for each test case:
+        #read test case parameters from stdin
+        board_size,max_slide=read_board_params()
+        #read board content from stdin
+        board=read_board(board_size)
+        
+        print_board(board,board_size) #só para testar
+
+
+
 
 if __name__=="__main__":
     main()
 
-#pypy3 C.py -m py_compile < test.txt
+#pypy3 main.py -m py_compile < test.txt
