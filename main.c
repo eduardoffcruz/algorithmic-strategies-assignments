0eb4cc011b552afd811beb514012b13265cd74c8
Eduardo Cruz <eduardoffcruz@gmail.com>
2021-02-27 13:46:50 +0000

    Update main.c

--- main.c
+++ main.c
@@ -18,44 +18,66 @@ int* slideRight(int** board, int size):
         after_elem_count+=after_count
     }
         
-    return before_elem_count,after_elem_count,board_after  
-
-int isCandidate(int *occ){
-    int len=11; //2^11=2048
+    return before_elem_count,after_elem_count,board_after  ;
 
 
+
+
+int isCandidate(int occ[]){
+    int count=0,len=11; //2^11=2048
+    for(int i=0;i<len;i++){
+        count=count+(occ[i]*(int)pow(2,i));
+    }
+    return isBase2(count);
 }
-
 int isBase2(n){
     return (n & (n-1) == 0) && n != 0;
 }
+int *initArray(int size){
+    int i;
+    int *arr=(int*)malloc(size*sizeof(int));
+    for(i=0;i<size;i++){
+        arr+i*sizeof(int)=0 //zero out
+    }
+    return arr;
+}
 
-int *intArray()
+void getMinSlide(int *board,int board_size){
+    int answer;
+    answer= recursiveTries();
+    if(answer==-1)
+        printf("no solution\n");
+    else
+        printf("%d\n",answer);
+}
 
-
-int main(void){
+void main(void){
     //read input
     int row,i,num_testcases,board_size,max_slide;
     int *board,*occ;
     int x;
 
-
     scanf("%d",&num_testcases);
     for(i=0; i<num_testcases;i++){
         scanf("%d %d",&board_size,&max_slide);
-        board=(int*)malloc(board_size*board_size*sizeof(int));
-        occ=(int*)malloc(11*sizeof(int)); //occurrence array
+        board=initArray(board_size*board_size);
+        occ=initArray(11); //occurrence array
         for(row=0;row<board_size*board_size;row++){
             scanf("%d",&x);
-            (board+row*sizeof(int))=x
+            (board+row*sizeof(int))=x;
             //printf("-> %d\n",*(board+row*sizeof(int)));
             if(x!=0){
-                if(occ[log2(x)])
-                occ[log2(x)]+=1
+                occ[log2(x)]++;
             }
         }
         //neste momento temos board
-
-        //free(board)
+        //check if board isn't impossible 
+        if(isCandidate(occ)){
+            getMinSlide(board,board_size);
+        }
+        else{
+            printf("no solution\n");
+        }
+        free(board); free(occ);
     }
 }
