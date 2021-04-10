da979a7227e5a7db9979ff5fc5c12f1faed577d2
Rodrigo Sobral <rodrigosobral@sapo.pt>
2021-04-10 19:40:09 +0100

    almost :coffee:

--- ProblemB/main.py
+++ ProblemB/main.py
@@ -11,38 +11,41 @@ def mod_add(a: int, b: int, mod: int): return (mod_abs(a, mod) + mod_abs(b, mod)
 def mod_sub(a: int, b: int, mod: int): return mod_add(a, -b, mod)
 
 def buildBlock(board:list, width:int, lower_lim:int, higher_lim:int):
-    aux= board.copy()
+    aux = [row[:] for row in board]
     for height in range(lower_lim, higher_lim): aux[width][height]=1
     return aux
 
-#   Returns True if there's some block
-def sameHeights(board:list, width:int, height:int):
-    for i in range(width-1, -1, -1):
-        if board[i][height-1]==0 and board[i][height]==1: return True
-    return False
 
 def generateCombinations(board:list, n:int, h:int, H:int, width:int, lower_lim: int, higher_lim:int, backwarding:bool):
-    if lower_lim<0 or higher_lim>=H or n>=H: 
-        return 0
-    elif width==0: 
-        return generateCombinations(buildBlock(board, 0, 0, h), n, h, H, 1, 1, h-1, False)
+    if lower_lim<=0 or higher_lim>H or n<3 or n>=H: 
+        return -1
     elif width==n-1:
         clone_board= buildBlock(board, width, 0, h)
         if board[width-1][h-1]==clone_board[width][h-1]: 
             global combinations_counter
-            combinations_counter+= mod_add(combinations_counter, 1, 1000000007)
+            combinations_counter= mod_add(combinations_counter, 1, 1000000007)
             return 1
         else: 
             return 0
     else:
-        for growing in range(lower_lim, higher_lim+1):
-            if not backwarding:
+        if not backwarding:
+            for growing in range(lower_lim, higher_lim+1):
+                if growing+h>H: 
+                    backwarding= True
+                    break
                 clone_board= buildBlock(board, width, growing, growing+h)
-                result= generateCombinations(clone_board, n, h, H, width+1, growing+1, growing+h, False)
-                if result==0: 
-                    return generateCombinations(clone_board, n, h, H, width+1, lower_lim-1, higher_lim-1, True)
-            else:
-                return generateCombinations(clone_board, n, h, H, width+1, growing-h, growing-1, True)
+                result= generateCombinations(clone_board, n, h, H, width+1, growing+1, growing+h-1,False)
+                if result==0 or result==-1:
+                    backwarding= True
+                    break
+        if backwarding:
+            for degrowing in range(higher_lim, lower_lim-2, -1):
+                if degrowing-h<0:
+                    return
+                clone_board= buildBlock(board, width, degrowing-h+1, degrowing+1)
+                result= generateCombinations(clone_board, n, h, H, width+1, degrowing-h+1, degrowing, True)
+                if result==-1:
+                    break
         
 
 
@@ -55,7 +58,7 @@ def main() -> None:
         combinations_counter=0
         [n, h, H] = [int(parameter) for parameter in readln().split(' ')]
         board= [[0]*H for i in range(n)]
-        generateCombinations(board, n, h, H, 0, 0, 0, False)
+        generateCombinations(buildBlock(board, 0, 0, h), n, h, H, 1, 1, h-1, False)
         outln(combinations_counter)
         
 
@@ -70,8 +73,7 @@ if __name__=='__main__':
 #   2
 #   2
 #   4
-#   5
-#   4
+#   54
 #   819
 
 #   test_case2.txt OUTPUT
