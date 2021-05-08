bd346725b9c11f05c3119d034808c6c6aac9ab9f
Eduardo Cruz <eduardoffcruz@gmail.com>
2021-05-08 11:31:56 +0100

    código + limpo e  pequena otimização

--- ProblemC/main.c
+++ ProblemC/main.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 1000
-//max num of vertexs == 1000
-int adj_list[MAX][(MAX)*2-1]; //fixed size adj list
-int adj_mat[MAX][MAX]; //for minimum spanning tree calculation
-int scc[MAX/2][MAX+1];
+#define MAX 1000 //max num of vertexs == 1000
+
+struct aresta{
+    int v1;
+    int v2;
+    int dist; //distance between v1---->v2 
+};
+
+//tarjan
+int adj_list[MAX][MAX]; //fixed size adj list
+int scc[MAX][MAX+1]; 
 int low[MAX];
 int dfs[MAX]; 
+//kruskal
+int distance[MAX][MAX]; //used for faster minimum spanning tree calculation (this structure is similar to an adjecency matrix but with weights instead of 0s and 1s)
+struct aresta arestas[MAX*MAX]; 
+int set[MAX], rank[MAX];
+//para cada caso de teste:
 int n; //number of POIs (vertex qnt)
 int t; //dfs counter
 int scc_counter; //includes subsets with one vertex only (one circuit is made out of 2 vertexes)
@@ -16,13 +27,8 @@ int max_scc_vertex_count;
 int longest_lane_lenght;
 int total_length;
 
-struct aresta{
-    int v1;
-    int v2;
-    int dist; //weight
-};
-
-int stack[MAX]; //fixed size stack 
+/*-----fixed size stack---------*/
+int stack[MAX]; 
 int on_stack[MAX];
 int stack_counter=0;
 
@@ -32,7 +38,7 @@ void push(int v){
 int pop(){
     return stack[--stack_counter];
 }
-
+/*------------------------------*/
 int _min(int a, int b){
     if(a<b) return a;
     return b;
@@ -53,52 +59,46 @@ void print_sccs(){
 
 void tarjan(int v){
     int i,w;
-    //int total_scc_len=0;
+
     int scc_vertex_counter=0;
     low[v-1]=dfs[v-1]=t;
-    t++;
-    push(v);
-    on_stack[v-1]=1;//true
+    t++; //dfs counter
+    push(v); on_stack[v-1]=1;//true
     
     for(i=1;i<=adj_list[v-1][0];i++){
-        w=adj_list[v-1][i*2-1]; //vizinho de V
+        w=adj_list[v-1][i]; //vizinho de v
         //printf("v %d tem como vizinho w %d\n",v,w);
         if(dfs[w-1]==-1){
-            //printf("0->v=%d w=%d dist: %d\n",v,w,adj_list[v-1][i*2]);
             tarjan(w);
             low[v-1]=_min(low[v-1],low[w-1]);
         }
-        else if(on_stack[w-1]){
+        else if(on_stack[w-1]){ //allows to check if vertex is on stack in constant time!
             //backedge!
             low[v-1]=_min(low[v-1],dfs[w-1]);
         }
     }
     if(low[v-1]==dfs[v-1]){
-        //printf("v %d é raiz (%d)\n",v,low[v-1]);
         //^se esta condição se verificar v é raiz de uma componente fortemente conexa
+        //printf("v %d é raiz (low:%d)\n",v,low[v-1]);
         do{
             w=pop();
             on_stack[w-1]=0;//false
             scc[scc_counter][scc_vertex_counter+1]=w;
             scc_vertex_counter++;
-            //total_scc_len+=d;
         }while(w!=v);
         scc[scc_counter][0]=scc_vertex_counter;
         scc_counter++;
         if(scc_vertex_counter>1){
             //if scc is a circuit (has more that 1 vertexes)
             circuit_counter++;
-            //total_length+=total_scc_len;
-            //if(total_scc_len>longest_lane_lenght)
-                //longest_lane_lenght=total_scc_len;
-            if(scc_vertex_counter>max_scc_vertex_count) //para guardar o nr de POIs que o maior SCC tem
+            if(scc_vertex_counter>max_scc_vertex_count) //para guardar o nr de POIs (vertexes) que o maior SCC tem
                 max_scc_vertex_count=scc_vertex_counter;
         }
 
     }
 }
 
-void link(int a, int b,int* rank, int *set){
+void link(int a, int b){
     if(rank[a-1]>rank[b-1]){
         set[b-1]=a;
     }
@@ -110,15 +110,15 @@ void link(int a, int b,int* rank, int *set){
     }
 }
 
-int find(int v, int *set){
+int find(int v){
     if(set[v-1]!=v){
-        set[v-1]=find(set[v-1],set);
+        set[v-1]=find(set[v-1]);
     }
     return set[v-1];
 }
 
-void _union(int v1, int v2, int *rank, int *set){
-    link(find(v1,set),find(v2,set),rank,set);
+void _union(int v1, int v2){
+    link(find(v1,set),find(v2,set));
 }
 
 int compareDist(const void *a1, const void *a2) {
@@ -129,18 +129,15 @@ int compareDist(const void *a1, const void *a2) {
 
 int calculate_min_lengths_of_scc(int index){
     //1º, para cada scc q seja um circuito ordenamos in ascending order as arestas
-    //const int max_arestas_qnt=scc[index][0]*scc[index][0];
-    //struct aresta arestas[max_arestas_qnt]; 
-    struct aresta arestas[MAX]; 
-    //int set[scc[index][0]], rank[scc[index][0]];
-    int set[MAX], rank[MAX];
+    //struct aresta arestas[MAX*MAX]; 
+    //int set[MAX], rank[MAX];
     int i,j,counter=0;
     int dist,len=0;
-    //printf("check ok1\n");
+
     //preencher array com arestas
     for(i=1;i<=scc[index][0];i++){
         for(j=1;j<=scc[index][0];j++){
-            if((dist=adj_mat[scc[index][i]-1][scc[index][j]-1])!=0){
+            if((dist=distance[scc[index][i]-1][scc[index][j]-1])!=0){ //constant time to acess distance between vertexes
                 arestas[counter].v1=scc[index][i]; //v1
                 arestas[counter].v2=scc[index][j]; //v2
                 arestas[counter].dist=dist;
@@ -155,16 +152,15 @@ int calculate_min_lengths_of_scc(int index){
         set[scc[index][i]-1]=scc[index][i]; rank[scc[index][i]-1]=0;
     }
     
-    //ordenar array com arestas por distancia entre vertices
+    //ordenar array com arestas por distancia entre vertices (ascending order (non-descending order))
     qsort(arestas,counter,sizeof(struct aresta),compareDist);
 
     for(i=0;i<counter;i++){
         //para cada aresta
-        //printf("v %d w %d dist %d\n",arestas[i].v1,arestas[i].v2,arestas[i].dist);
-        if(find(arestas[i].v1,set)!=find(arestas[i].v2,set)){
+        if(find(arestas[i].v1)!=find(arestas[i].v2)){
             //printf("bateu1\n");
             len+=arestas[i].dist;
-            _union(arestas[i].v1,arestas[i].v2,rank,set);
+            _union(arestas[i].v1,arestas[i].v2,rank);
         }
     }
 
@@ -176,8 +172,8 @@ void calculate_all_scc_min_lengths(){
     int i,len;
     for(i=0;i<scc_counter;i++){
         if(scc[i][0]>1){
-            //circuit
-            len=calculate_min_lengths_of_scc(i);
+            //se o SCC tiver mais de 1 POI (vertex) é um circuito
+            len=calculate_min_lengths_of_scc(i); //kruskal algorithm call
             total_length+=len;
             if(len>longest_lane_lenght)
                 longest_lane_lenght=len;
@@ -202,17 +198,16 @@ int main(void){
         t=1;
         scanf("%d %d %d",&n,&m,&q);
         for(i=0;i<n;i++){
-            memset(adj_list[i],0,(n-1)*2*sizeof(int)); //clear adj_list
-            memset(adj_mat[i],0,n*sizeof(int)); //clear adj_mat
+            memset(adj_list[i],0,n*sizeof(int)); //clear adj_list
+            memset(distance[i],0,n*sizeof(int)); //clear distance matrix
             low[i]=dfs[i]=-1;
             on_stack[i]=0;
         }
         for(i=0;i<m;i++){
             scanf("%d %d %d",&a,&b,&d);
             v_count=++adj_list[a-1][0];
-            adj_list[a-1][v_count*2-1]=b; //vertex b
-            adj_list[a-1][v_count*2]=d; //distance from a to b
-            adj_mat[a-1][b-1]=d;
+            adj_list[a-1][v_count]=b; //vertex b
+            distance[a-1][b-1]=d;
         }
         for(i=1;i<=n;i++)
             if(dfs[i-1]==-1)
